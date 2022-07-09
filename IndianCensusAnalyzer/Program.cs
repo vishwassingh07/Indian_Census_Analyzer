352057eee5eb36a303c4b9f238eaa80482deeceb
Vishwas <baghel.vishwas0@gmail.com>
2022-07-09 16:55:47 +0530

    [Vishwas]add: Indian Census Analyzer Program Added

---
+++ IndianCensusAnalyzer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStatesCensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Indian State Census Analyser Program");
+            Console.ReadLine();
+        }
+    }
+}
